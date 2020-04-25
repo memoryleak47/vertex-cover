3380c678c294e70cc011215348879cbeef1473ea
memoryleak47 <memoryleak47@protonmail.ch>
2020-04-25 20:22:50 +0200

    did things

--- Graph_random.hs
+++ Graph_random.hs
@@ -17,18 +17,11 @@ empty i = Graph (map (\x -> map (\y -> False) [0..x-1]) [0..i-1])
 clique :: Int -> Graph
 clique i = Graph (map (\x -> map (\y -> True) [0..x-1]) [0..i-1])
 
-randbool :: IO Bool 
-randbool = randomRIO (False, True)
+purerandomlist :: StdGen -> Int -> [Bool]
+purerandomlist g x = take x $ randoms g
 
-randomlist x = take x $ randoms (mkStdGen 5) :: [Bool]
+randomgraph :: IO StdGen -> Int -> IO Graph
+randomgraph g i = fmap (\z -> Graph (map (\x -> purerandomlist z x) [0..i-1])) g
 
---randomgraph i = Graph (map (\x -> map (\y -> randbool) [0..x-1]) [0..i-1])
-
-randomgraph :: Int -> Graph
-randomgraph i = Graph (map (\x -> randomlist x) [0..i-1])
-
-
---take (x-1) randomlist
-
-
--- main = randomgraph 4 >> show >> putStrLn
+main = do
+    ((fmap show (randomgraph newStdGen 5)) >>= putStrLn)
